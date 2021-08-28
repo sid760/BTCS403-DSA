0eb1d21390bcb24dd4aee5dd0dd424124820387e
Sid760 <mishrasiddharth760@gmial.com>
2021-08-28 15:21:18 +0530

    subjectsArray

---
+++ subjectsArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   int n;
+    cout << "Enter the no. of subjects in your current semester: " << endl;
+    cin >> n;
+    string subjects[n];
+    cout << "Enter " << n << " subjects: " << endl;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> subjects[i];
+    }
+    cout << "Your subjects are: " << endl;
+    for(int i = 0; i < n; i++) {
+        cout << subjects[i] << endl;
+    }
+    
+    return 0;
+}
